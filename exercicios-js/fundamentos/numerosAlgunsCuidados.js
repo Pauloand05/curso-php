b7d1c25f82d4752f7faa8ab0b2e0928024895a71
Pauloand05 <pauloandre050619@gmail.com>
2025-01-13 21:43:46 -0300

    Alguns cuidados com numeros em JS

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7 / 0) // retorna infinity
+console.log("10" / 2) //retorna os 5 mesmo sendo uma string
+console.log("Show!" * 2) // nao funciona
+//console.log(10.toString())  nao funciona para numero
+console.log((10.345).toFixed(2))// funciona normalmente
