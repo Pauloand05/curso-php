2ef82d239f3ea21aa37df52ec1dcf6b2c4812ad9
Pauloand05 <pauloandre050619@gmail.com>
2025-01-15 15:27:56 -0300

    Objeto JS

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Celular'
+prod1.preco = 4989.90
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Máquina de lavar',
+    preco: 1990.90
+}
+
+console.log(prod2)
