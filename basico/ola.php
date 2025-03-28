9a416a1e64a2a42eedc1cc5f21e2d1326e1dc452
Pauloand05 <pauloandre050619@gmail.com>
2025-03-27 22:31:38 -0300

    Olá PHP

---
+++ basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá PHP</div>
+
+<?php 
+
+echo "Olá \n";
+echo "Mundo!";
+
+?>
+
+<?= "Outra forma de me 'expressar'!" ?>
+
+<?php
+phpinfo(); // Informações do PHP
--- exercicio.php
+++ exercicio.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
     <link rel="stylesheet" href="assets/css/exercicio.css">
     <title>Exercício</title>
@@ -13,16 +13,19 @@
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="verde">Sem Formatação</a>
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
+            class="verde">Sem formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
-            
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
     <footer class="rodape">
-        COD3R & ALUNOS © <?= date('Y') ?>
+        COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
 </html>
