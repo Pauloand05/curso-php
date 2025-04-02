c766a17a0ee8ef9076a04bf8dd4ced1759363fb5
Pauloand05 <pauloandre050619@gmail.com>
2025-04-01 22:40:27 -0300

    Tipo float PHP

---
+++ tipos/float.php
@@ -0,0 +1,12 @@
+<div class="titulo">Tipo float</div>
+
+<?php
+echo 1.1, '<br>';
+
+var_dump(1.0); // var_dump mostra o tipo e o valor da variável
+echo '<br>';
+
+echo PHP_FLOAT_MAX, '<br>'; // mostra o maior float suportado pelo PHP
+echo PHP_FLOAT_MIN, '<br>'; // mostra o menor float suportado pelo PHP
+echo 1.2e3, '<br>'; // 1.2 * 10^3 = 1200
+echo 13E-3, '<br>'; // 13 * 10^-3 = 0.013
