81a221e21165e6abaf397768d18e013aff1ad82c
Pauloand05 <pauloandre050619@gmail.com>
2025-04-01 22:11:48 -0300

    Tipo inteiro PHP

---
+++ tipos/int.php
@@ -0,0 +1,15 @@
+<div class="titulo">Tipo inteiro</div>
+
+<?php
+echo 11; // inteiro
+echo '<br>';
+
+
+var_dump(11); // inteiro
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>'; // maior inteiro suportado
+echo PHP_INT_MIN, '<br>'; // menor inteiro suportado
+echo 011, '<br>'; // octal (base 8) - 9
+echo 0b11, '<br>'; // binário (base 2) - 3
+echo 0x17ac, '<br>'; // hexadecimal (base 16) - 17172
